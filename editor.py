149871720fa2460b0d24cd733178c82c5e599b99
praj1812 <prajakti18@gmail.com>
2017-04-23 22:50:17 -0500

    search using 'noun' instead of 'NN' or 'NNS' etc.

--- editor.py
+++ editor.py
@@ -9,6 +9,7 @@ import tkinter.simpledialog as tksd
 
 import synonym_search
 import part_speech_search
+import part_of_speech_to_tag
 # import search_pop
 # import take_input
 #
@@ -111,7 +112,6 @@ def search_synonyms():
     text.tag_remove("tag", "1.0", END)
 
     ment = tksd.askstring("Dialog (String)", "Enter your search:", parent=master)
-    print(ment)
     # ment = pop_up.MyDialog.ok()
     search_word = str(ment)
 
@@ -136,31 +136,39 @@ def search_synonyms():
     master.update_idletasks()
 
 
-
-def part_speach():
+def part_speech():
     text.tag_remove("tag", "1.0", END)
 
     ment = tksd.askstring("Dialog (String)", "Enter your grammar search:", parent=master)
-    print(ment)
+    print("ment: ", ment)
     part_word = str(ment)
+
+    print('part_word', part_word)
     
     # get the text from the text editor
     the_text = text.get("1.0", END)
-    
-    r_dict = part_speech_search.make_dict(part_word, the_text, the_text)
-    print(r_dict)
-    
-    if part_word not in r_dict:
-        messagebox.showinfo("Part of Speech", "Part of speech word not found.")
-        return
-    else:
-        word_pspeech = r_dict[part_word]
 
-    for item in word_pspeech:
-        print("word: " + str(item[0]) + " line: " + str(item[1]) + " , " + " column: " + str(item[2]))
-        
-        text.tag_add("tag", str(item[1]) + "." + str(item[2]), str(item[1]) + "." + str(len(item[0]) + item[2]))
-        text.tag_config("tag", background="orange", foreground="black")
+    #TODO: Change file name?
+    pos_acr_list = part_speech_search.part_of_speech_to_tag(part_word)
+    print("pos_acr_list: ", pos_acr_list)
+    r_dict = part_speech_search.make_dict(the_text, the_text)
+
+    print("r_dict: ", r_dict)
+
+    for pos_acr in pos_acr_list:
+        # if pos_acr not in r_dict.keys():
+        #     messagebox.showinfo("Part of Speech", "Part of speech word not found.")
+        #     return
+        if pos_acr in r_dict.keys():
+            #else:
+            word_speech = r_dict[pos_acr]
+            print('word_speech:', word_speech)
+
+            for item in word_speech:
+                print("word: " + str(item[0]) + " line: " + str(item[1]) + " , " + " column: " + str(item[2]))
+
+                text.tag_add("tag", str(item[1]) + "." + str(item[2]), str(item[1]) + "." + str(len(item[0]) + item[2]))
+                text.tag_config("tag", background="orange", foreground="black")
 
 # print(the_text)
 
@@ -230,7 +238,7 @@ search_menu.add_command(label="Entity Analysis", command = sentiment)
 # mEntry = Entry(search_menu, textvariable=ment).pack()
 grammar_menu = Menu(menu)
 menu.add_cascade(label="Grammar", menu=grammar_menu)
-grammar_menu.add_cascade(label="Part of Speech", command=part_speach)
+grammar_menu.add_cascade(label="Part of Speech", command=part_speech)
 
 extra_menu = Menu(menu)
 menu.add_cascade(label="Extra", menu=extra_menu)
--- part_of_speech_to_tag.py
+++ part_of_speech_to_tag.py
@@ -1,5 +1,31 @@
+# def part_of_speech_to_tag(part_of_speech):
+#     tag = {'adjective': 'ADJ', 'adposition': 'ADP', 'adverb': 'ADV', 'conjunction': 'CONJ', 'determiner': 'DET',
+#            'article': 'DET', 'noun': 'NOUN', 'number': 'NUM', 'particle': 'PRT', 'pronoun': 'PRON', 'verb': 'VERB',
+#            'punctuation': '.', 'other': 'X'}
+#     return tag(part_of_speech);
+
+
 def part_of_speech_to_tag(part_of_speech):
-    tag = {'adjective': 'ADJ', 'adposition': 'ADP', 'adverb': 'ADV', 'conjunction': 'CONJ', 'determiner': 'DET',
-           'article': 'DET', 'noun': 'NOUN', 'number': 'NUM', 'particle': 'PRT', 'pronoun': 'PRON', 'verb': 'VERB',
-           'punctuation': '.', 'other': 'X'}
-    return tag(part_of_speech);
+    tag = {'conjunction': ['CC'], 'number': ['CD'], 'determiner': ['DT'], 'preposition': ['IN'],
+           'adjective': ['JJ', 'JJR', 'JJS'], 'noun': ['NN', 'NNS', 'NNP', 'NNPS'], 'pronoun': ['PRP', 'PRP$'],
+           'adverb': ['RB', 'RBR', 'RBS'], 'verb': ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']}
+    part_of_speech_acr = tag.get(part_of_speech)
+    if part_of_speech_acr is not None:
+        return part_of_speech_acr
+
+# 4.	EX	Existential there
+# 5.	FW	Foreign word
+# 10.	LS	List item marker
+# 11.	MD	Modal
+# 16.	PDT	Predeterminer
+# 17.	POS	Possessive ending
+#
+# 23.	RP	Particle
+# 24.	SYM	Symbol
+# 25.	TO	to
+# 26.	UH	Interjection
+#
+# 33.	WDT	Wh-determiner
+# 34.	WP	Wh-pronoun
+# 35.	WP$	Possessive wh-pronoun
+# 36.	WRB	Wh-adverb
